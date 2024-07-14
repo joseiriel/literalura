11b7127d96bc3ea856a52b74b5c8cba5db2744bf
José Iriel de Freitas Martins <jose.iriel.martins@gmail.com>
2024-07-14 16:53:23 -0300

    Commit inicial

---
+++ src/test/java/com/github/joseiriel/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joseiriel.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
